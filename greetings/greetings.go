c02f5a31175dab9b53e47b8bf881a3d60d02d73e
kodai <kodaiadvance@icloud.com>
2023-11-20 13:01:09 +0900

    create a module

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,10 +1,54 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+)
 
 //Hello returns a greeting for the named person.
-func Hello(name string) string {
-	//Return a greeting that embeds the name in a message.
-	message := fmt.Sprintf("Hi, %v. Welcome.", name)
-	return message
+func Hello(name string) (string, error) {
+	// If no name was given, return an error with a message.
+	if name == "" {
+		return "", errors.New("empty name")
+	} 
+	// Create a message using a random format.
+	// message := fmt.Sprintf(rendomFormat(), name) 
+	message := fmt.Sprintf(randomFormat(), name)
+	return message, nil
+	}
+
+// randomFormat returns one of a set greeting messages.
+// The returned message is selected at random.
+
+// Hellos returns a map that associates each of the named people
+// with a greeting message.
+func Hellos(names []string) (map[string]string, error) {
+	// A map to associate names with messages.
+	messages := make(map[string]string)
+	// Loop through the received slice of names, calling
+	// the Hello function to get a message for each name.
+	for _, name := range names {
+		message, err := Hello(name)
+		if err != nil {
+			return nil, err
+		}
+		// In the map, associate the received message with
+		// the name.
+		messages[name] = message
+	}
+	return messages, nil
+}
+
+func randomFormat() string {
+	// A slice of message formats.
+	formats := []string {
+		"Hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"Hali, %v! Well met!",
+	}
+
+	// Returns a ramdomly selected message format by specifing
+	// a random index for the slice of formats.
+	return formats[rand.Intn(len(formats))]
 }
